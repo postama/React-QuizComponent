c05bdabeaa1b9a202486404968028fb1d8a83576
Adam Postma <postama29@gmail.com>
2018-12-05 22:32:08 -0700

    handle wrong answer

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -2,8 +2,17 @@ import React, { Component } from 'react';
 import QuizQuestionButton from './QuizQuestionButton';
 
 class QuizQuestion extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { incorrectAnswer: false };
+    }
     handleClick(buttonText) {
-        if (buttonText === this.props.quiz_question.answer) this.props.showNextQuestionHandler();
+        if (buttonText === this.props.quiz_question.answer) {
+            this.setState({ incorrectAnswer: false });
+            this.props.showNextQuestionHandler();
+        } else {
+            this.setState({ incorrectAnswer: true });
+        }
     }
     render() {
         return (
@@ -18,6 +27,7 @@ class QuizQuestion extends Component {
                         )}
                     </ul>
                 </section>
+                {this.state.incorrectAnswer ? <p className='error'>sorry, that's not right</p> : null}
             </main>
         )
     }
